2556f9869f3871f0f0cb7c2e9bef4a8504a59ba5
Gregor Müllegger <gregor@muellegger.de>
2010-10-24 17:42:36 +0200

    Removing media files url from example/urls.py.

--- example/urls.py
+++ example/urls.py
@@ -17,16 +17,8 @@ handler404 = 'example.urls.handle404'
 handler500 = 'example.urls.handle500'
 
 
-SORTED_M2M_MEDIA = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-    'src', 'sortedm2m', 'media', 'sortedm2m')
-
-
 urlpatterns = patterns('',
-    url(r'^media/sortedm2m/(.*)$', 'django.views.static.serve',
-        {'document_root': SORTED_M2M_MEDIA}),
-    url(r'^media/(.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT}),
+    url(r'^media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^admin/', include(admin.site.urls), name="admin"),
     url(r'^', include('staticfiles.urls')),
 )
