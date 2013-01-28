b2be6247ae8aa24970047da6222d0281e1ab227f
Gregor Müllegger <gregor@muellegger.de>
2013-01-28 22:17:30 +0100

    Updating manage.py to work with Django 1.5

--- test_project/manage.py
+++ test_project/manage.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
-from django.core.management import execute_manager
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import settings
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
