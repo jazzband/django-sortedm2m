4403a424d747a3420c61ee7007270240ec23e125
Gregor Müllegger <gregor@muellegger.de>
2013-02-27 00:54:43 +0100

    Mentioning Python 3 support in the changelog.

--- setup.py
+++ setup.py
@@ -75,6 +75,11 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
     ],
     install_requires = [],
 )
