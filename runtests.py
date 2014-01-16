0aa54a8ca2342e50e47f9b24074c4fa1620194b4
Gregor Müllegger <gregor@muellegger.de>
2014-01-16 20:37:00 +0100

    Improving test infrastructure and also testing with Django 1.6

--- runtests.py
+++ runtests.py
@@ -7,20 +7,37 @@ import os, sys
 parent = os.path.dirname(os.path.abspath(__file__))
 
 sys.path.insert(0, parent)
-sys.path.insert(0, os.path.join(parent, 'test_project'))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_project.settings")
 
+
+import django
 from django.core.management import call_command
 
 
-def runtests(*args):
-    test_apps = args or [
+if django.VERSION < (1, 6):
+    default_test_apps = [
         'sortedm2m_tests',
         'sortedm2m_field',
         'sortedm2m_form',
         'south_support',
+        'south_support_new_model',
+        'south_support_new_field',
+        'south_support_custom_sort_field_name',
     ]
-    call_command('test', *test_apps)
+else:
+    default_test_apps = [
+        'sortedm2m_tests.sortedm2m_field',
+        'sortedm2m_tests.sortedm2m_form',
+        'sortedm2m_tests.south_support',
+        'sortedm2m_tests.south_support.south_support_new_model',
+        'sortedm2m_tests.south_support.south_support_new_field',
+        'sortedm2m_tests.south_support.south_support_custom_sort_field_name',
+    ]
+
+
+def runtests(*args):
+    test_apps = args or default_test_apps
+    call_command('test', *test_apps, verbosity=1)
 
 
 if __name__ == '__main__':
--- test_settings.py
+++
@@ -1,1 +0,0 @@
-from test_project.settings import *
