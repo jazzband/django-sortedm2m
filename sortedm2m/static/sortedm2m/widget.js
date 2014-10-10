402dfd41cc0f14e2bc713a791bc731244cd48268
Rubén Díaz <outime@users.noreply.github.com>
2014-10-10 14:59:31 +0300

    Partially solves issue #20
    When an inline admin form is added, sortedm2m functionality is broken (i.e. values are not saved until next full page load). This patch solves the issue.

--- sortedm2m/static/sortedm2m/widget.js
+++ sortedm2m/static/sortedm2m/widget.js
@@ -4,29 +4,41 @@ if (jQuery === undefined) {
 
 (function ($) {
     $(function () {
-        $('.sortedm2m').parents('ul:first').each(function () {
-            $(this).addClass('sortedm2m');
-            var checkboxes = $(this).find('input[type=checkbox]');
+
+        $.fn.prepare_ul = function () {
+            this.addClass('sortedm2m');
+            var checkboxes = this.find('input[type=checkbox]');
             var id = checkboxes.first().attr('id').match(/^(.*)_\d+$/)[1];
             var name = checkboxes.first().attr('name');
             checkboxes.removeAttr('name');
-            $(this).before('<input type="hidden" id="' + id + '" name="' + name + '" />');
+            this.before('<input type="hidden" id="' + id + '" name="' + name + '" />');
             var that = this;
             var recalculate_value = function () {
                 var values = [];
-                $(that).find(':checked').each(function () {
+                that.find(':checked').each(function () {
                     values.push($(this).val());
                 });
                 $('#' + id).val(values.join(','));
             }
             recalculate_value();
             checkboxes.change(recalculate_value);
-            $(this).sortable({
+            this.sortable({
                 axis: 'y',
                 //containment: 'parent',
                 update: recalculate_value
             });
-        });
+            return this;
+        };
+
+        var iterate_ul = function() {
+            $('.sortedm2m').parents('ul').each(function () {
+                $(this).prepare_ul();
+            });
+        }
+
+        $(".add-row a").click(iterate_ul);
+
+        iterate_ul();
 
         $('.sortedm2m-container .selector-filter input').each(function () {
             $(this).bind('input', function() {
