0a450fc58761d942b6f447ed828b7c495601f00c
Rohith Asrk <rohith.asrk@gmail.com>
2017-03-21 14:31:41 +0530

    Fix errors

--- example/testapp/models.py
+++ example/testapp/models.py
@@ -9,9 +9,12 @@ class Car(models.Model):
     def __unicode__(self):
         return self.plate
 
+
 class m2mprint:
+
     def __unicode__(self):
         return unicode(self.car) + " in " + unicode(self.parkingarea)
+
 
 class ParkingArea(models.Model):
     name = models.CharField(max_length=50)
