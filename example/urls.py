7c0dd372e1d3db6de53d2558c693dc2d34a42e11
Jannis Leidel <jannis@leidel.info>
2021-03-10 14:25:49 +0100

    More compat fixes.

--- example/urls.py
+++ example/urls.py
@@ -1,8 +1,8 @@
 import django.views.static
 from django.conf import settings
-from django.conf.urls import include, url
 from django.contrib import admin
 from django.http import HttpResponse
+from django.urls import include, re_path
 
 import example.testapp.views
 
@@ -10,24 +10,32 @@ admin.autodiscover()
 
 
 def handle404(request, exception):
-    return HttpResponse('404')
+    return HttpResponse("404")
 
 
 def handle500(request):
-    return HttpResponse('500')
+    return HttpResponse("500")
 
 
-handler404 = 'example.urls.handle404'
-handler500 = 'example.urls.handle500'
+handler404 = "example.urls.handle404"
+handler500 = "example.urls.handle500"
 
 
 if django.VERSION < (1, 9):
-    urlpatterns = [url(r'^admin/', include(admin.site.urls), name="admin")]
+    urlpatterns = [re_path(r"^admin/", include(admin.site.urls), name="admin")]
 else:
-    urlpatterns = [url(r'^admin/', admin.site.urls, name="admin")]
+    urlpatterns = [re_path(r"^admin/", admin.site.urls, name="admin")]
 
 urlpatterns += [
-    url(r'^media/(.*)$', django.views.static.serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^parkingarea/(?P<pk>\d+)/$', example.testapp.views.parkingarea_update, name='parkingarea'),
-    url(r'^', include('django.contrib.staticfiles.urls')),
+    re_path(
+        r"^media/(.*)$",
+        django.views.static.serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+    re_path(
+        r"^parkingarea/(?P<pk>\d+)/$",
+        example.testapp.views.parkingarea_update,
+        name="parkingarea",
+    ),
+    re_path(r"^", include("django.contrib.staticfiles.urls")),
 ]
