440d1e8a8a431afe08f70ef227ffee9d12496264
Clinton Blackburn <clinton.blackburn@gmail.com>
2024-08-11 15:45:55 -0700

    Replaced Firefox with Chromium for Selenium tests

--- test_project/qunit-runner.py
+++ test_project/qunit-runner.py
@@ -1,20 +1,21 @@
 import os
 import time
 
-from selenium import webdriver  # pylint:disable=no-name-in-module
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 URL = "file://" + ROOT + "/qunit.html"
 TIMEOUT = 5
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.set_page_load_timeout(TIMEOUT)
 driver.get(URL)
 
 time.sleep(TIMEOUT)
 
 xpath = '//div[@id="qunit-testresult-display"]/span[@class="failed"]'
-element = driver.find_element_by_xpath(xpath)
+element = driver.find_element(by=By.XPATH, value=xpath)
 count = int(element.text)
 assert count == 0
 driver.close()
